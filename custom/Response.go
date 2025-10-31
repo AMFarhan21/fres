1cf4fc5e963cc208fd833a4d7f24cd7ac5f39ebe
AMFarhan21 <AMFarhan2102@gmail.com>
2025-10-31 16:30:11 +0800

    feat: create default and custom response

--- custom/Response.go
+++ custom/Response.go
@@ -1,13 +1,13 @@
-package fres
+package custom
 
 type SuccessResponse struct {
 	Success bool        `json:"success"`
 	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 type ErrorResponse struct {
 	Success bool        `json:"success"`
 	Message string      `json:"message"`
-	Error   interface{} `json:"error"`
+	Error   interface{} `json:"error,omitempty"`
 }
