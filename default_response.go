f2f73476101e8cade013547a743f34587c6be0aa
AMFarhan21 <AMFarhan2102@gmail.com>
2025-11-02 14:53:37 +0800

    fix: add omitempty on success again but create another type that doesnt have omitempty on the success

--- default_response.go
+++ default_response.go
@@ -4,64 +4,64 @@ type response struct{}
 
 var Response = response{}
 
-func (r response) StatusOK(data interface{}) SuccessResponse {
-	return SuccessResponse{
+func (r response) StatusOK(data interface{}) DefaultSuccessResponse {
+	return DefaultSuccessResponse{
 		Success: true,
 		Message: "Request successful",
 		Data:    data,
 	}
 }
 
-func (r response) StatusCreated(data interface{}) SuccessResponse {
-	return SuccessResponse{
+func (r response) StatusCreated(data interface{}) DefaultSuccessResponse {
+	return DefaultSuccessResponse{
 		Success: true,
 		Message: "Resource created successfully",
 		Data:    data,
 	}
 }
 
-func (r response) StatusBadRequest(error interface{}) ErrorResponse {
-	return ErrorResponse{
+func (r response) StatusBadRequest(error interface{}) DefaultErrorResponse {
+	return DefaultErrorResponse{
 		Success: false,
 		Message: "Invalid request body",
 		Error:   error,
 	}
 }
 
-func (r response) StatusUnauthorized(error interface{}) ErrorResponse {
-	return ErrorResponse{
+func (r response) StatusUnauthorized(error interface{}) DefaultErrorResponse {
+	return DefaultErrorResponse{
 		Success: false,
 		Message: "Unauthorized",
 		Error:   error,
 	}
 }
 
-func (r response) StatusForbidden(error interface{}) ErrorResponse {
-	return ErrorResponse{
+func (r response) StatusForbidden(error interface{}) DefaultErrorResponse {
+	return DefaultErrorResponse{
 		Success: false,
 		Message: "You do not have permission to access this resource",
 		Error:   error,
 	}
 }
 
-func (r response) StatusNotFound(error interface{}) ErrorResponse {
-	return ErrorResponse{
+func (r response) StatusNotFound(error interface{}) DefaultErrorResponse {
+	return DefaultErrorResponse{
 		Success: false,
 		Message: "Resource not found",
 		Error:   error,
 	}
 }
 
-func (r response) StatusConflict(error interface{}) ErrorResponse {
-	return ErrorResponse{
+func (r response) StatusConflict(error interface{}) DefaultErrorResponse {
+	return DefaultErrorResponse{
 		Success: false,
 		Message: "Conflict",
 		Error:   error,
 	}
 }
 
-func (r response) StatusInternalServerError(error interface{}) ErrorResponse {
-	return ErrorResponse{
+func (r response) StatusInternalServerError(error interface{}) DefaultErrorResponse {
+	return DefaultErrorResponse{
 		Success: false,
 		Message: "Internal server error",
 		Error:   error,
